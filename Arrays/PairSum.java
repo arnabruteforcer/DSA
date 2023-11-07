e3a0faf3222cd81c95c064eee8b33c7782a4a6a1
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2023-11-07 23:52:55 +0530

    Update PairSum.java

--- Arrays/PairSum.java
+++ Arrays/PairSum.java
@@ -6,6 +6,55 @@ in non decreasing order
 
 **/
 
+public class Solution {
+
+    // Approach 1
+    public static List<int[]> pairSum(int[] arr, int s) {
+        ArrayList<int[]> pairs = new ArrayList<>();
+        // a simple brute force approach comparing each pair and adding them to the result if the target meets    
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[i] + arr[j] == s) {
+                    int[] pair = new int[2];
+                    if( arr[i]<arr[j] ) {
+                        pair[0] = arr[i];
+                        pair[1] = arr[j]; 
+                    } else {
+                        pair[0] = arr[j];
+                        pair[1] = arr[i];
+                    }
+                    pairs.add(pair);
+                }
+           }
+        }
+
+      // sorting the pairs      
+      Collections.sort(pairs, new Comparator<int[]>() {
+        @Override
+        public int compare(int[] o1, int[] o2) {
+            if (o1[0] > o2[0]) {
+                return 1;
+            } else if (o1[0] == o2[0]) {
+                if(o1[1] > o2[1]) {
+                    return 1;
+                } else if(o1[1] < o2[1]) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            } else {
+                return -1;
+            }
+        }
+        });
+        return pairs;
+    }
+// Time complexity - O(n^2)
+// Space complexity - O(1)        
+        
+
+
+
 // Approach 2 
 public static List<int[]> pairSum(int[] arr, int s) {
         ArrayList<int[]> pairs = new ArrayList<>();
