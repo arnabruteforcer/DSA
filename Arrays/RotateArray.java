1a3c1e0c954a68fa89ebb7a6663a151d19b29a46
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2023-11-08 01:43:23 +0530

    Update RotateArray.java

--- Arrays/RotateArray.java
+++ Arrays/RotateArray.java
@@ -9,8 +9,12 @@ Input: arr[] = {3, 4, 5, 6, 7, 1, 2}, d=2
 Output: 5 6 7 1 2 3 4   
 **/
 
+import java.io.*;
+import java.util.* ;
 
-// Solution 1
+public class Solution {
+
+// Solution 1 - store and shift
 public static ArrayList<Integer> rotateArray(ArrayList<Integer> arr, int k) {
         int[] temp = new int[k];
         for(int i=0; i<k; i++) {
@@ -33,3 +37,43 @@ public static ArrayList<Integer> rotateArray(ArrayList<Integer> arr, int k) {
 
 // Time - O(n) for traversing all arrays in a single way 
 // Space - O(k) for temp array
+
+
+
+
+
+// Approach 2 - rotation by reversal
+public static ArrayList<Integer> rotateArray(ArrayList<Integer> arr, int k) {
+        
+        int n = arr.size();
+        k = k % n;
+        
+        // reverse the complete array k=2
+        reverse(arr,0,n); // 1 2 3 4 5 --> 5 4 3 2 1
+        // reverse the n-k elements so that they maintain the correct order 
+        // and are shifted
+        reverse(arr, 0, n-k); // ( 3 4 5 ) 2 1  
+        // arrange the k-elements at the end in order as left-rotation by 'k' means
+        // the initial k elements will shift towards the end
+        reverse(arr, n-k, n); // 3 4 5 1 2
+        return arr;
+    }
+
+
+
+    // reverse the array till mid 
+    private static void reverse(ArrayList<Integer> arr, int l, int r) {
+        int mid = (r - l)/2;
+        int i=0;
+        while( i<mid ) {
+            int temp = arr.get(l+i);
+            arr.set(l+i, arr.get(r-i-1));
+            arr.set(r-i-1,temp);
+            i++;
+        }
+    }
+
+    // Time complexity - O(n)
+    // Space complexity - O(1) 
+
+}
