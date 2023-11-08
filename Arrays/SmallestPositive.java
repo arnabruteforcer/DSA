9da175b17fbe65e1303572b80b9bc60fd440931c
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2023-11-08 22:29:16 +0530

    Update SmallestPositive.java

--- Arrays/SmallestPositive.java
+++ Arrays/SmallestPositive.java
@@ -26,15 +26,32 @@ public class Solution {
 	// Time complexity - O(nlogn)
 	// Space complexity - O(1)
 
+	
+
+ 	// Approach 2
+	public static int firstMissing(int[] arr, int n) {
+
+		// space for storing the n +ve numbers
+		int[] numCount = new int[n];
+
+		// we traverse through the array
+		for(int i=0; i<arr.length; i++) {
+			// store the count of each +ve number in array
+			if(arr[i] > 0 && arr[i]<=n ) {
+				numCount[arr[i]-1]++;
+			}
+		}
 
 
+		// traverse the array among n numbers
+		for(int i=0; i<numCount.length; i++) {
+			if(numCount[i] == 0) {
+				return i+1;
+			}
+		}
 
-
-
-
-
-
-
-
-  
+		return n+1;
+	}
+	// Time complexity - O(n)
+	// Space complexity - O(n)  
 }
