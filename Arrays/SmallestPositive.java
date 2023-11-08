8e00d4b44f35b5ca5b27695bee70b78b6e2d3548
arnabruteforcer <30342252+arnabruteforcer@users.noreply.github.com>
2023-11-08 22:06:18 +0530

    Create SmallestPositive.java

---
+++ Arrays/SmallestPositive.java
@@ -0,0 +1,40 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+	// Approach 1
+	public static int firstMissing(int[] arr, int n) {
+		// The array is sorted initially
+		Arrays.sort(arr);
+
+		// the smallest positive number is set to 1
+		int smallestPositive = 1;
+
+		// we traverse through the array
+		for(int i=0; i<arr.length; i++) {
+			// if the number is present we increment the counter to the next +ve 
+			if(arr[i] == smallestPositive) {
+				smallestPositive++;
+			// if the next available number is not present and we exceed it,
+			// we return the number 	
+			} else if (arr[i] > smallestPositive) {
+				break;
+			}
+		}
+		return smallestPositive;
+	}
+	// Time complexity - O(nlogn)
+	// Space complexity - O(1)
+
+
+
+
+
+
+
+
+
+
+
+  
+}
